5847aa612d6fb4993312a0d484f1ab18f161be5f
yongde2900 <com414141@gmail.com>
2020-10-25 16:44:04 +0800

    feat: add filter function

--- routes/models/home.js
+++ routes/models/home.js
@@ -4,8 +4,16 @@ const Record = require('../../models/Record')
 const Category = require('../../models/Category')
 
 router.get('/', async (req, res) => {
-    const records = await Record.find().populate('category', 'name icon').lean()
+    const _id = req.query._id
+    let records
+    if (!_id) {
+        records = await Record.find().populate('category', 'name icon').lean()
+    } else {
+        const categoryName = await Category.findById(_id).lean()
+        records = await Record.find({ category: categoryName }).populate('category', 'name icon').lean()
+    }
     const categories = await Category.find().lean()
-    res.render('index', {records, categories})
+    res.render('index', { records, categories })
 })
+
 module.exports = router
