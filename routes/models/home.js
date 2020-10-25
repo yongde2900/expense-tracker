08302f580b7fd1c4638218a7e09f3e1b478fe3ce
yongde2900 <com414141@gmail.com>
2020-10-25 16:51:32 +0800

    feat: add total amount

--- routes/models/home.js
+++ routes/models/home.js
@@ -6,14 +6,21 @@ const Category = require('../../models/Category')
 router.get('/', async (req, res) => {
     const _id = req.query._id
     let records
+    let totalAmount = 0
     if (!_id) {
         records = await Record.find().populate('category', 'name icon').lean()
+        for(record of records){
+            totalAmount += record.amount
+        }
     } else {
         const categoryName = await Category.findById(_id).lean()
         records = await Record.find({ category: categoryName }).populate('category', 'name icon').lean()
+        for(record of records){
+            totalAmount += record.amount
+        }
     }
     const categories = await Category.find().lean()
-    res.render('index', { records, categories })
+    res.render('index', { records, categories, totalAmount })
 })
 
 module.exports = router
