1ca107d49721d435dfcafeca1748a7bcdf2ea782
yongde2900 <com414141@gmail.com>
2020-10-25 16:10:59 +0800

    feat: add edit function

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const router = express.Router()
 const home = require('./models/home')
 const expense = require('./models/expense')
 
+router.use(methodOverride('_method'))
 router.use('/', home)
 router.use('/expense', expense)
 
--- routes/models/expense.js
+++ routes/models/expense.js
@@ -4,13 +4,13 @@ const Record = require('../../models/Record')
 const router = express.Router()
 
 router.get('/new', async(req, res) => {
-    categories = await Category.find().lean()
+    const categories = await Category.find().lean()
     res.render('new',{categories})
 })
 
 router.post('/', async(req, res) => {
     const newExpense = req.body
-    categories = await Category.find().lean()
+    const categories = await Category.find().lean()
     targetId = categories.find( category => category.name === newExpense.category)._id
     newExpense.category = targetId
     return Record.create(newExpense)
@@ -18,4 +18,29 @@ router.post('/', async(req, res) => {
         .catch( error => console.log(error))
 })
 
+router.get('/:_id/edit', async(req ,res) => {
+    const _id =req.params._id
+    const categories = await Category.find().lean()
+    Record.findById(_id)
+        .lean()
+        .then( record => {
+            res.render('edit', { record, categories})
+        })
+        .catch(error => console.log(error))
+})
+
+router.put('/:_id', async(req, res) => {
+    const _id = req.params._id
+    const newExpense = req.body
+    const categories = await Category.find().lean()
+    targetId = categories.find( category => category.name === newExpense.category)._id
+    newExpense.category = targetId
+    return Record.findById(_id)
+        .then( record => {
+            record = Object.assign(record , newExpense)
+            return record.save()
+        })
+        .then( () => res.redirect('/'))
+        .catch( error => console.log(error))
+})
 module.exports = router
