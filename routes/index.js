7d884861dbda53094ba23f84df8d90fc210e3eb3
yongde2900 <com414141@gmail.com>
2020-11-12 21:21:50 +0800

    feat: add login page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const methodOverride = require('method-override')
 const router = express.Router()
 const home = require('./models/home')
 const expense = require('./models/expense')
+const users = require('./models/users')
 
 router.use(methodOverride('_method'))
 router.use('/', home)
 router.use('/expense', expense)
+router.use('/users', users)
 
 module.exports = router
