60710e25f7a1f0d09d7f2153d3d75030017fe47e
yongde2900 <com414141@gmail.com>
2020-11-12 22:16:40 +0800

    feat: add bcryptjs

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Record'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => console.log('mongodb is error!'))
--- routes/models/users.js
+++ routes/models/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/User')
 const router = express.Router()
+const saltRounds = 10
 
 router.get('/login', (req, res) => {
     res.render('login')
@@ -7,6 +10,30 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const {name, email, password, confirmPassword} = req.body
+    if(!name || !email || !password || !confirmPassword){
+        console.log('必填')
+        return res.render('register', {name, email, password ,confirmPassword})
+    }
+    if(password !== confirmPassword){
+        console.log('不相符')
+        return res.render('register', {name, email, password ,confirmPassword})
+    }
+    User.findOne({email})
+        .then(user => {
+            if(user){
+                console.log('這個 Email 已經註冊過了。')
+                return res.render('register', {name, email, password ,confirmPassword})
+            }
+            return bcrypt.genSalt(saltRounds)
+                .then(salt => bcrypt.hash(password, salt))
+                .then(hash => User.create({name, email, password: hash}))
+                .then(() => res.redirect('/users/login'))
+                .catch(err => console.log(err))
+        })
 })
 
 module.exports = router
